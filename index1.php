d479f8d5ab765c917f4ae71e5780c78738e6ac29
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2021-12-24 15:00:03 +0100

    affichage des notes et lien sur le bouton +

--- index1.php
+++ index1.php
@@ -5,6 +5,51 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Mes Notes</title>
     <link rel="stylesheet" href="style.css">
+    <script>
+      $(document).ready( function()
+                          {
+                            $.get( "index1Back.php",
+                                    function( data, status )
+                                    {
+                                      let tableau = JSON.parse( data );
+                                      // console.log( tableau );
+                                      let chaine = "";
+
+                                      for ( ligne of tableau )
+                                      {
+                                        chaine += "<div class='tacheSimple'>";
+                                        chaine += "<h2>"+ligne['titre']+"</h2>";
+                                        chaine += "<h3>"+ligne['description']+"</h3>";
+                                        chaine += "<div class='dateCreation'><p>"+ligne['dateCreation']+"</p></div>";
+                                        chaine += "</div>";
+                                      }
+
+                                      $( "#notesSimple" ).append( chaine );
+                                    }   
+                                  );
+
+                                  $.get( "index1EpingleBack.php",
+                                    function( data, status )
+                                    {
+                                      let tableau = JSON.parse( data );
+                                      // console.log( tableau );
+                                      let chaine = "";
+
+                                      for ( ligne of tableau )
+                                      {
+                                        chaine += "<div class='tacheEpingle'>";
+                                        chaine += "<h2>"+ligne['titre']+"</h2>";
+                                        chaine += "<h3>"+ligne['description']+"</h3>";
+                                        chaine += "<div class='dateCreation'><p>"+ligne['dateCreation']+"</p></div>";
+                                        chaine += "</div>";
+                                      }
+
+                                      $( "#notesEpingle" ).append( chaine );
+                                    }   
+                                  );
+                          }
+                        );
+    </script>
   </head>
 
   <body>
@@ -15,32 +60,14 @@
     <main>
         <div class="contenu" id="contenu">
 
-            <div class="notesEpingle" id="notesEpingle">
-                <div class="tacheEpingle">
-                    <h2>Titre</h2>
-                    <h3>Description</h3>
-                    <div class="dateCreation"><p>Date de Creation</p></div>
-                </div>
-
-                <div class="tacheEpingle">
-                    <h2>Titre</h2>
-                    <h3>Description</h3>
-                    <div class="dateCreation"><p>Date de Creation</p></div>
-                </div>
-            </div>
+            <div class="notesEpingle" id="notesEpingle"><p style="color:white;">Epinglé</p></div>
 
             <div class="separation" id="separation"></div>
 
-            <div class="notesSimple" id="notesSimple">
-                <div class="tacheSimple">
-                    <h2>Titre</h2>
-                    <h3>Description</h3>
-                    <div class="dateCreation"><p>Date de Creation</p></div>
-                </div>
-            </div>
+            <div class="notesSimple" id="notesSimple"></div>
 
             <div id="finContenu">
-                <button class="boutonAdd">+</button>
+                <button onclick="window.location.href='creationNote.php';" class="boutonAdd">+</button>
             </div>
 
         </div>
---
+++ index1Back.php
@@ -0,0 +1,18 @@
+<?php
+    require_once "ressources.php";
+
+    $query = "SELECT * FROM notes WHERE epingle=0;";
+    
+    $mysqli = new mysqli($servername, $username, $password, $database);
+    $res = $mysqli->query( $query );
+    // $ligne = $res->fetch_assoc();
+
+    $tableauData = [];
+
+    while ( $ligne = $res->fetch_assoc())
+        $tableauData[] = $ligne;
+
+    print( json_encode( $tableauData ) );
+
+    $mysqli->close();
+?>
